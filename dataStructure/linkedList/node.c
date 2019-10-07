2ef2bfa7f8dfe4506d34b6a876dc15ad5511278f
scottxiong <github.scott.bear@gmail.com>
2019-10-07 23:23:35 +0800

    update

--- dataStructure/linkedList/node.c
+++ dataStructure/linkedList/node.c
@@ -2,7 +2,7 @@
 * @Author: scottxiong
 * @Date:   2019-10-07 22:05:48
 * @Last Modified by:   scottxiong
-* @Last Modified time: 2019-10-07 23:01:37
+* @Last Modified time: 2019-10-07 23:15:51
 */
 #include <stdio.h>
 #include <stdlib.h>
@@ -42,7 +42,44 @@ void unshift(node_t **head, int val){
     *head = new_node;
 }
 
+//Removing the first item (popping from the list)
+int shift(node_t **head){
+     int retval = -1;
+     node_t * next_node = NULL;
 
+     if (*head == NULL) {
+         return -1;
+     }
+
+     next_node = (*head)->next;
+     retval = (*head)->val;
+     free(*head);
+     *head = next_node;
+
+     return retval;
+}
+
+int pop(node_t *head){
+   int retval = 0;
+    /* if there is only one item in the list, remove it */
+    if (head->next == NULL) {
+        retval = head->val;
+        free(head);
+        return retval;
+    }
+
+    /* get to the second to last node in the list */
+    node_t * current = head;
+    while (current->next->next != NULL) {
+        current = current->next;
+    }
+
+    /* now current points to the second to last item of the list, so let's remove current->next */
+    retval = current->next->val;
+    free(current->next);
+    current->next = NULL;
+    return retval;
+}
 int main(int argc, char const *argv[])
 {
   	node_t *head = NULL;
@@ -62,7 +99,7 @@ int main(int argc, char const *argv[])
     printf("after unshift:\n");
     unshift(&head,4);
     printNodes(head);
-	return 0;
+	  return 0;
 }
 
 /*
