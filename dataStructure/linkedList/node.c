f08632fd70b7714f4f85b2fd519412d840c48956
scottxiong <github.scott.bear@gmail.com>
2019-10-07 23:02:01 +0800

    fmt

--- dataStructure/linkedList/node.c
+++ dataStructure/linkedList/node.c
@@ -2,14 +2,14 @@
 * @Author: scottxiong
 * @Date:   2019-10-07 22:05:48
 * @Last Modified by:   scottxiong
-* @Last Modified time: 2019-10-07 22:38:35
+* @Last Modified time: 2019-10-07 23:01:37
 */
 #include <stdio.h>
 #include <stdlib.h>
 
 typedef struct node{
-	int value;
-	struct node *next;
+  	int value;
+  	struct node *next;
 }node_t;
 
 //loop linked list
@@ -23,18 +23,18 @@ void printNodes(node_t *head){
 
 //Adding an item to the end of the list
 void push(node_t *head,int val){
-       node_t * current = head;
-       while(current->next!=NULL){
-            current=current->next;
-       }
-       current->next = malloc(sizeof(node_t));
-       current->next->value = val;
-       current->next->next = NULL;
+     node_t * current = head;
+     while(current->next!=NULL){
+          current=current->next;
+     }
+     current->next = malloc(sizeof(node_t));
+     current->next->value = val;
+     current->next->next = NULL;
 }
 
 //Adding an item to the beginning of the list (pushing to the list)
 void unshift(node_t **head, int val){
-	node_t * new_node;
+	  node_t * new_node;
     new_node = malloc(sizeof(node_t));
 
     new_node->value = val;
@@ -45,17 +45,17 @@ void unshift(node_t **head, int val){
 
 int main(int argc, char const *argv[])
 {
-	node_t *head = NULL;
-	head = malloc(sizeof(node_t));
-	if (head == NULL) {
-		return 1;
-	}
-	head->value =1;
-	head->next = malloc(sizeof(node_t));
-	head->next->value = 2;
-	head->next->next = NULL;
-	printf("pushed before:\n");
-	printNodes(head);
+  	node_t *head = NULL;
+  	head = malloc(sizeof(node_t));
+  	if (head == NULL) {
+  		return 1;
+  	}
+  	head->value =1;
+  	head->next = malloc(sizeof(node_t));
+  	head->next->value = 2;
+  	head->next->next = NULL;
+  	printf("pushed before:\n");
+  	printNodes(head);
     printf("after push:\n");
     push(head,3);
     printNodes(head);
