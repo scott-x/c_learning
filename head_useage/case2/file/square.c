1260085d5bf9983e6307a4e2f5aaa0b4c103566b
scottxiong <github.scott.bear@gmail.com>
2019-10-24 17:48:11 +0800

    update

---
+++ head_useage/case2/file/square.c
@@ -0,0 +1,11 @@
+/*
+* @Author: scottxiong
+* @Date:   2019-09-27 06:28:14
+* @Last Modified by:   scottxiong
+* @Last Modified time: 2019-09-27 06:29:42
+*/
+#include "square.h"
+
+int square(int a) {
+	return a*a;
+}
---
+++ head_useage/case2/main.c
@@ -0,0 +1,15 @@
+/*
+* @Author: scottxiong
+* @Date:   2019-09-27 02:55:02
+* @Last Modified by:   scottxiong
+* @Last Modified time: 2019-10-24 17:40:23
+*/
+#include <stdio.h>
+#include "file/square.h"
+
+int main(int argc, char const *argv[])
+{
+	int result = square(5);
+	printf("result is %d\n", result); //result is 25
+	return 0;
+}
